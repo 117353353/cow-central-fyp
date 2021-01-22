0b42394da56c6836d6324b86342ad4c711997a3e
117353353 <32956864+117353353@users.noreply.github.com>
2021-01-22 18:33:05 +0000

    iteration 3 add milk recording form

--- components/MilkRecording.js
+++ components/MilkRecording.js
@@ -1,35 +1,84 @@
 import React, { useState } from "react"
-import { StyleSheet } from "react-native"
-import { Card, Input, Text } from "react-native-elements"
+import { StyleSheet, ScrollView } from "react-native"
+import { Card, Input, Text, Button } from "react-native-elements"
+import {db} from "../firebase"
 
 const styles = StyleSheet.create({
 
 })
 
 function MilkRecording() {
+    const [cowId, setCowId] = useState(0)
+    const [date, setDate] = useState(0)
+    const [milkProduced, setMilkProduced] = useState(0)
+    const [protein, setProtein] = useState(0)
+    const [butterfat, setButterfat] = useState(0)
+    const [cellCount, setCellCount] = useState(0)
+    const [notes, setNotes] = useState("")
+
+    function addMilkRecording() {
+        db.collection("milkRecordings").doc().set({
+            cowId: cowId,
+            date: date,
+            milkProduced: milkProduced,
+            protein: protein,
+            butterfat: butterfat,
+            cellCount: cellCount,
+            notes: notes
+        }).then(() => {
+            alert("SUCCESS!")
+        }).catch(error => {
+            alert(error.message)
+        })
+    }
 
     return (
-        <Text>Under Contruction</Text>
+        <ScrollView>
+            <Input
+                style={styles.textInput}
+                onChangeText={text => setCowId(text)}
+                value={cowId}
+                label="Tag Number"
+                keyboardType="number-pad"
+            />  
+            <Input
+                style={styles.textInput}
+                onChangeText={text => setMilkProduced(text)}
+                value={milkProduced}
+                label="Volume of Milk Produced"
+                keyboardType="number-pad"
 
+            />  
+            <Input
+                style={styles.textInput}
+                onChangeText={text => setProtein(text)}
+                value={protein}
+                label="Protein"
+                keyboardType="number-pad"
+            />  
+            <Input
+                style={styles.textInput}
+                onChangeText={text => setButterfat(text)}
+                value={butterfat}
+                label="Butterfat"
+                keyboardType="number-pad"
+            />  
+            <Input
+                style={styles.textInput}
+                onChangeText={text => setCellCount(text)}
+                value={cellCount}
+                label="Somatic Cell Count"
+            />  
+            <Input
+                style={styles.textInput}
+                onChangeText={text => setNotes(text)}
+                value={notes}
+                label="Notes"
+                multiline={true}
+            />  
+            <Button title="Add Recording" onPress={addMilkRecording} />
+        </ScrollView>
     )
 }
 
 export default MilkRecording
-
-        {/* <Card>
-            <Input
-                style={styles.textInput}
-                onChangeText={text => setTagNum(text)}
-                value={tagNum}
-                label="Tag Number"
-            />  
-               
-            <Input
-                style={styles.textInput}
-                onChangeText={text => setDob(text)}
-                value ={dob}
-                label="Date of Birth"
-            />  
-  
-            <Button title="Add Cow" onPress={add} />          
-        </Card> */}
